dce551e16177d51bcab56f9fce17340d55274790
James Prentice <jamesprenticez@gmail.com>
2022-08-27 18:12:38 +1200

    chapter 4

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,7 @@ def index():
         {
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
-    }
+        }
     ]
 
     return render_template('index.html', title='Home', user=user, posts=posts)
@@ -25,8 +25,9 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
+        
     return render_template('login.html', title='Sign In', form=form)
